b9c4519542f3ec0bd8c1d4d4df4acadc42b7c4a4
Omer Viner <omer.viner@mail.huji.ac.il>
2017-12-05 22:39:20 +0200

    *fiels

---
+++ drawing_graph.py
@@ -0,0 +1,52 @@
+import random
+import pickle
+from flask import make_response
+
+
+def get_random_id(num_bits=16):
+    return hex(random.randint(0, 2 ** num_bits))[2:].zfill(num_bits / 4)
+
+
+class User(object):
+    def __init__(self, user_id, mail=None):
+        self.user_id = user_id
+        self.mail = mail
+        self.drawings_list = []
+
+
+class Node(object):
+    def __init__(self, node_id, user_id, drawing, parent_node_id, state = "in progress"):
+        self.node_id = node_id
+        self.user_id = user_id
+        self.parent_node_id = parent_node_id
+        self.drawing = drawing
+        self.state = state #options: "in_progress", "done", "cancelled"
+        self.children_nodes_ids = []
+
+    def export_to_dict(self):
+        return {
+            "node_id" : self.node_id,
+            "user_id": self.user_id,
+            "parent_node_id": self.parent_node_id,
+            "drawing": self.drawing,
+            "state": self.state,
+            "children_nodes_ids": self.children_nodes_ids
+            }
+
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, user_id, drawing, parent_node_id, state):
+        assert parent_node_id in self.nodes or parent_node_id is None
+
+        node_id = str(len(self.nodes))
+        new_node = Node(node_id, user_id, drawing, parent_node_id, state)
+        if parent_node_id is not None:
+            self.nodes[parent_node_id].children_nodes_ids.append(node_id)
+        self.nodes[node_id] = new_node
+        return new_node
+
+    def export_to_dict(self):
+        return {node_id : node.export_to_dict() for node_id, node in self.nodes.items()}
