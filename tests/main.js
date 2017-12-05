5d8946f609f4a7c38ed88eff16ae324409f67309
syoels <syoels@gmail.com>
2017-12-05 20:12:54 +0200

    start working on drag

--- tests/main.js
+++ tests/main.js
@@ -9,14 +9,21 @@ var force = d3.layout.force()
     .size([width, height])
     .on("tick", tick);
 
+var drag = d3.behavior.drag()
+    .origin(function(d) { return d; })
+    .on("dragstart", dragstarted)
+    .on("drag", dragged)
+    .on("dragend", dragended);
+
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
 
 svg.append("rect")
     .attr("width", width)
     .attr("height", height)
-    .attr("fill", bgColor);
+    .attr("fill", bgColor)
+    //.call(drag); //TODO: not working yet :(
 
 var link = svg.selectAll(".link"),
     node = svg.selectAll(".node");
@@ -24,14 +31,14 @@ var link = svg.selectAll(".link"),
 api.getTree(
     //on success
     function (resp) {
-        console.log('success! got tree:');
-        console.log(resp);
         root = resp;
         saveImagesAsPatternsInCanvas(svg, root);
         update();
     },
     //on failure
     function (resp) {
-        console.log('fail :(');
+        console.log('Request for tree failed :(');
         console.log(resp);
     }, true);
+
+
