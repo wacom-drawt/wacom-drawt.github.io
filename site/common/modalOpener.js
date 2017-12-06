c9f8814dda128b0759443108e915d98990818239
Noam Palombo <noampalombo@Noams-MacBook-Pro-2.local>
2017-12-06 12:47:22 +0200

    integrate graph with editor fix

--- site/common/modalOpener.js
+++ site/common/modalOpener.js
@@ -10,10 +10,12 @@ ModalOpener = function(){
 		settings = settings || this.defaultSettings;
 		switch(settings.type){
 			case this.types.EDITOR:
+			WILL.init(1600, 1000, settings.node.drawing);
+			console.log("will initiated");
 				console.log('openning editor modal');
 				api = api || new ApiService();
 				console.log('node is: ');
-				console.log(node);
+				console.log(settings.node);
 				api.branchFrom(settings.node, function(response){
 					console.log('got branch response:');
 					console.log(response);
