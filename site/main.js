9d91ecdce8706113303081c6e78cbe14701a94f0
syoels <syoels@gmail.com>
2017-12-06 17:19:25 +0200

    enable sample flag

--- site/main.js
+++ site/main.js
@@ -60,7 +60,7 @@ var link = svg.selectAll(".link"),
 	node = svg.selectAll(".node");
 
 function init() {
-	var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
+	var url_string = window.location.href
 	var url = new URL(url_string);
 	var c = url.searchParams.get("sample");
 	isMock = !!c;
