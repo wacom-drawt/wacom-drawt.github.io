01f80a68b36c20d7b86912c5d2ded24b56eae63d
syoels <syoels@gmail.com>
2017-12-06 17:17:56 +0200

    enable sample flag

--- site/main.js
+++ site/main.js
@@ -2,74 +2,80 @@ var api = new ApiService();
 var modalOpener = new ModalOpener();
 
 var width = window.innerWidth,
-    height = window.innerHeight,
-    bgColor = "#3f3f3f",
-    centered,
-    root;
+	height = window.innerHeight,
+	bgColor = "#3f3f3f",
+	centered,
+	root;
 
 var force = d3.layout.force()
-    .size([width, height])
-    .on("tick", tick);
+	.size([width, height])
+	.on("tick", tick);
 
 var active = d3.select(null);
 
 var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .call(d3.behavior.zoom().on("zoom", zoomed))
-    .append("g")
+	.attr("width", width)
+	.attr("height", height)
+	.call(d3.behavior.zoom().on("zoom", zoomed))
+	.append("g")
 
 var drag = d3.behavior.drag()
-    .origin(function(d) { return d; })
-    .on("dragstart", dragstarted)
-    .on("drag", dragged)
-    .on("dragend", dragended);
+	.origin(function (d) {
+		return d;
+	})
+	.on("dragstart", dragstarted)
+	.on("drag", dragged)
+	.on("dragend", dragended);
 
-function zoomed(){
-    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
-    link.style("stroke-width", 1 + (d3.event.scale*0.000000001))
-    node.style("stroke-width", 1 + (d3.event.scale*0.000000001));
+function zoomed() {
+	svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+	link.style("stroke-width", 1 + (d3.event.scale * 0.000000001))
+	node.style("stroke-width", 1 + (d3.event.scale * 0.000000001));
 }
 
 function dragstarted(d) {
-    d3.event.sourceEvent.stopPropagation();
-    d3.select(this).classed("dragging", true);
-    //update();
+	d3.event.sourceEvent.stopPropagation();
+	d3.select(this).classed("dragging", true);
+	//update();
 
 }
 
 function dragged(d) {
-    d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+	d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
 
-    //update();
+	//update();
 }
 
 function dragended(d) {
-    d3.select(this).classed("dragging", false);
-    update();
+	d3.select(this).classed("dragging", false);
+	update();
 }
 
 svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", bgColor);
+	.attr("width", width)
+	.attr("height", height)
+	.attr("fill", bgColor);
 
 var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
+	node = svg.selectAll(".node");
 
 function init() {
-    api.getTree(
-        //on success
-        function (resp) {
-            root = resp;
-            saveImagesAsPatternsInCanvas(svg, root);
-            update();
-        },
-        //on failure
-        function (resp) {
-            console.log('Request for tree failed :(');
-            console.log(resp);
-        }, false);
+	var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
+	var url = new URL(url_string);
+	var c = url.searchParams.get("sample");
+	isMock = !!c;
+	api.getTree(
+		//on success
+		function (resp) {
+			root = resp;
+			saveImagesAsPatternsInCanvas(svg, root);
+			update();
+		},
+		//on failure
+		function (resp) {
+			console.log('Request for tree failed :(');
+			console.log(resp);
+		}, isMock);
 }
 
 
