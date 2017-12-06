19a1b9369326269f4ccaa798eab9880112cedd66
syoels <syoels@gmail.com>
2017-12-06 13:58:23 +0200

    debugging post

--- site/tree.js
+++ site/tree.js
@@ -55,7 +55,7 @@ function ApiService() {
 			node_id: newNodeId,
 			drawing: imageURI
 		};
-		xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+		xhr.setRequestHeader("Access-Control-Allow-Headers", "*");
 		// xhr.setRequestHeader('Access-Control-Request-Method', 'POST');
 		xhr.setRequestHeader("Access-Control-Allow-Origin", '*');
 		// xhr.setRequestHeader('Access-Control-Request-Headers', 'Content-Type, Authorization');
