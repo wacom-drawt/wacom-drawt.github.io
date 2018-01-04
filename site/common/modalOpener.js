59b7b866937874fa3f248f4ef491587c9c920f97
Omer Viner <omer.viner@mail.huji.ac.il>
2018-01-05 00:03:05 +0200

    * canvas size fix

--- site/common/modalOpener.js
+++ site/common/modalOpener.js
@@ -8,7 +8,8 @@ ModalOpener = function(){
 	};
 	this.openModal = function(settings){
 		settings = settings || this.defaultSettings;
-		WILL.init(1600, 1000, settings.node.drawing);
+		var size = Math.min(window.innerWidth, window.innerHeight)*0.9;
+		WILL.init(size, size, settings.node.drawing);
 		console.log("will initiated");
 		switch(settings.type){
 			case this.types.EDITOR:
@@ -24,6 +25,8 @@ ModalOpener = function(){
 					var $opener = $("#modalOpener");
 					$opener.click();
 				})
+				$("#modal-content").css('width', size + 'px');
+				$("#modal-content").css('height', size + 'px');
 				break;
 			default:
 				console.log('Problem! requested unknown modal type');
