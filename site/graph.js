13a30ad797970f5a8e16b517adde892118f0f108
syoels <syoels@gmail.com>
2018-01-20 11:53:17 +0200

    try to add "if exists" test to defs in thumbnail function

--- site/graph.js
+++ site/graph.js
@@ -143,7 +143,7 @@ function color(d) {
 function saveImagesAsPatternsInCanvas(canvasObj, root) {
 	var data = flatten(root);
 	var svg = canvasObj;
-	var prevDefs = d3.select("svg > defs");
+	var prevDefs = svg.select("defs");
 	var defs = prevDefs.empty() ? svg.append("defs") : prevDefs;
 
 	defs
